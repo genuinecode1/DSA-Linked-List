4764b43b1f0b9c0ab6a908cde22db1f5b9b75d16
genuine_code <113706866+genuinecode1@users.noreply.github.com>
2026-04-28 21:36:32 +0530

    "3"

---
+++ LL_Deletion/4.Delete_end.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class LL_Node
+{
+public:
+    int data;
+    LL_Node *next;
+
+    LL_Node(int val)
+    {
+        this->data = val;
+        this->next = NULL;
+    }
+};
+
+LL_Node *delete_at_beginning(LL_Node *head)
+{
+    LL_Node *slow = head;
+    LL_Node *fast = head->next;
+    while (fast->next != NULL)
+    {
+        slow = slow->next;
+        fast = fast->next;
+    }
+    slow->next = NULL;
+    delete fast;
+    return head;
+}
+
+void traversal(LL_Node *head)
+{
+    LL_Node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+    cout << endl;
+}
+int main()
+{
+    int arr[5] = {1, 2, 3, 4, 5};
+    LL_Node *start = new LL_Node(arr[0]);
+    LL_Node *head = start;
+    for (int i = 1; i < 5; i++)
+    {
+        LL_Node *temp = new LL_Node(arr[i]);
+        start->next = temp;
+        start = start->next;
+    }
+
+    traversal(head);
+    head = delete_at_beginning(head);
+    traversal(head);
+
+    return 0;
+}
